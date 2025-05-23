88dbb1e91705599707b70668a63a50db13846702
olivazambranod@gmail.com <olivazambranod@gmail.com>
2025-05-23 09:19:56 -0700

    header y proy

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
+<?php 
+ include_once('header.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CBTis 152</title>
 </head>
 <body>
     Hola mundo 2
