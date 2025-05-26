10cf370f0a9b3e84daee1f7f92e38aaa32b8d4f5
AngelC <angelcoyacmo@gmail.com>
2025-05-26 08:45:41 -0700

    cambie el color del icono

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
         </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
-            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon" style="color: white; filter: invert(100%);"></span>
         </button>
 
     
