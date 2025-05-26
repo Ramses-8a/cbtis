b5ae7b1a434574c9ea6fa03c76c9152e23c222fe
olivazambranod@gmail.com <olivazambranod@gmail.com>
2025-05-26 00:22:02 -0700

    proyecto y vista del login

--- mostrar_proyectos.php
+++ mostrar_proyectos.php
@@ -4,23 +4,33 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    
+    <link rel="stylesheet" href="css/proyecto.css">
+    <title>Proyectos</title>
 </head>
 <body>
-    <h1>Proyectos de Programación</h1><br>
-    <div class="proyectos">
-         <?php foreach ($proyectos as $proyecto): ?>
-        <img src="img/<?=$proyecto['img_proyecto']?>" alt="" width="250px">    
-        <h1><?=$proyecto['nom_proyecto']?></h1>
-        <h2><?=$proyecto['descripcion']?></h2>
-        <h3><?=$proyecto['detalles']?></h3>
+
+        <div class="con_volver">
+        <a href="index.php" class="volver">
+            <img src="img/volver.webp" alt="Volver">
+        </a>
+        <h3>Proyectos</h3>
+        </div>
+
+    <main class="proyectos">
+        <?php foreach ($proyectos as $proyecto): ?>
+            <button class="card">
+                <img src="img/<?=$proyecto['img_proyecto']?>" alt="Proyecto">
+                <p><strong><?=$proyecto['nom_proyecto']?></strong></p>
+                <p><?=$proyecto['descripcion']?></p>
+                <p><?=$proyecto['detalles']?></p>
+            </button>
         <?php endforeach; ?>
-    </div>
-   
+    </main>
+
 </body>
 </html>
-
