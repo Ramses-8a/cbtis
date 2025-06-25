b41669a5d17364545b8abe5678dcab9c110d5f9c
olivazambranod@gmail.com <olivazambranod@gmail.com>
2025-06-25 08:28:26 -0700

    Diseño final

--- header.php
+++ header.php
@@ -79,9 +79,5 @@
         </div>
     </div>
 </nav>
-
-
- <!-- cdn de bootstrap NO BORRAR -->
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
